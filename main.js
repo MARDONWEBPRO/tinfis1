512bccca738a24f3fe1ab8e20cf4ca75dec7c746
ADMIN MAX <mardonspy11@gmail.com>
2022-09-23 17:40:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var btn1 = document.getElementById('btn1');
+var inputPassword5 = document.getElementById('inputPassword5');
+
+function slideCart() {
+    var div1 = document.createElement('div');
+    div1.style.width = "33%";
+    div1.style.height = "100vh";
+    div1.style.backgroundColor = "white";
+    div1.style.float = "right";
+}
